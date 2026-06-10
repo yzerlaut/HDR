4c717ea9d0a81cc4b7365fdfbb318f64d34e16c0
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-06-10 22:40:25 +0200

    up

--- build/process_figures.py
+++ build/process_figures.py
@@ -8,30 +8,38 @@ for fn in sys.argv[1:]:
     with open(fn, 'r') as f:
         text = str(f.read())
 
-    for S in text.split("\%\% beginFigure \%\%")[1:]:
+    for S in text.split("\%\%beginFigure\%\%")[1:]:
 
-        fig = S.split('}')[0].split('{')[1]
+        old_fig = S.split('}')[0].split('{')[1]
         key = S.split('}')[0].split('/')[-1].replace('.png','').replace('.svg','') # png or svg
+        print('Figure %i.' % n, key, old_fig)
 
-        S = "\%\% beginFigure \%\%"+S
+        S = "\%\%beginFigure\%\%"+S
 
-        old_fig_string = S.split("\%\% endFigure \%\%")[0]+\
-                                            "\%\% endFigure \%\%"
+        old_fig_string = S.split("\%\%endFigure\%\%")[0]+\
+                                    "\%\%endFigure\%\%"
 
+        # print(old_fig_string)
         new_fig_string = old_fig_string.replace(\
-                "\%\% beginFigure \%\%", "\\begin{figure}\n")
+                "\%\%beginFigure\%\%", "\\begin{figure}\n")
 
         new_fig_string = new_fig_string.replace(\
                 "\\textbf{", "\\textbf{\hypertarget{fig:%s}{Figure %i}. " % (key,n))
 
         new_fig_string = new_fig_string.replace(\
-                "\%\% endFigure \%\%", "\end{figure}")
+                "\%\%endFigure\%\%", "\end{figure}")
 
-        # print(new_fig_string)
-        text = text.replace("\href{%s}{Fig.}" % fig,
+        # new_fig_string = new_fig_string.replace(\
+                # "../Figures", "./temp/Figures")
+
+        new_fig_string = new_fig_string.replace(\
+                "includesvg", "includegraphics")
+
+        new_fig_string = new_fig_string.replace(\
+                ".svg", ".png")
+
+        text = text.replace("\href{%s}{Fig.}" % old_fig,
                             "\hyperlink{fig:%s}{Fig. %i}" % (key,n))
-
-        new_fig_string = new_fig_string.replace("svg", "png") # force png figs
 
         text = text.replace(old_fig_string, new_fig_string)
 
